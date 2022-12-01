0dfd35bee3080c7e13ff5b1e71e6289e836bfd02
Prashant Singh <52065013+PrashantTariyal@users.noreply.github.com>
2022-12-01 15:33:46 +0530

    Update char_code_at.js

--- 02_Day_Data_types/string_methods/char_code_at.js
+++ 02_Day_Data_types/string_methods/char_code_at.js
@@ -2,6 +2,6 @@
 
 string.charCodeAt(index)
 let string = '30 Days Of JavaScript'
-console.log(string.charCodeAt(3)) // D ASCII number is 51
+console.log(string.charCodeAt(3)) // D ASCII number is 68
 let lastIndex = string.length - 1
 console.log(string.charCodeAt(lastIndex)) // t ASCII is 116
